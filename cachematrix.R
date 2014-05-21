0bcdd9942543fcb0fb38eaf206703f9872e66a70
cstubbsphd <cstubbs@alum.mit.edu>
2014-05-21 08:47:24 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Set of functions that calculate matrix inverses.  Previously calculated 
 ## inverses are cached so they do not have to be recalculated (memoization)
 
-## Write a short comment describing this function
+## This function defines a list of functions (set, get, setinverse, getinverse)
+## to store the matrix and its inverse
+## eg:  amatrix <- makeCacheMatrix(c(1,2,3,4), nrow=2, ncol=2)
+## To modify the matrix, use
+##    amatrix$set(c(0,5,99,66), nrow=2, ncol=2)
+## To show the matrix, use
+##    amatrix$get()
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix.  It caches the inverse for
+## future use, and returns the cached inverse if available.  
+## eg:  inv.matrix <- cacheSolve(amatrix)
+## After a call to cacheSolve, the computed inverse can be returned with
+##    amatrix.getinverse()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
